4633cbf12a3fc38c06217a0d8d342a2bc63b427b
forown <lcmjjjj@gmail.com>
2021-09-22 23:55:16 +0900

    13 fix

---
+++ src/views/site/footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-footer padless color="primary" dark :footer="footer">
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      &copy; {{ new Date().getFullYear() }} — <strong>{{ footer }}</strong>
+    </v-col>
+  </v-footer>
+</template>
+<script>
+export default {
+  props: ['footer']
+}
+</script>
